61023c99222c190e0753afe8e441e2f50c7b3931
Sebastian Galindo <sglaindom2612@gmail.com>
2026-05-30 17:56:23 -0500

    Conectar app al proyecto Firebase real ays2026

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDummyKeyForDevelopment",
-  authDomain: "asistencias-uts.firebaseapp.com",
-  projectId: "asistencias-uts",
-  storageBucket: "asistencias-uts.firebasestorage.app",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:dummyid"
+  apiKey: "AIzaSyAgnAkBiYzbXc2lwCO4ZILkEIMmV7NmlNg",
+  authDomain: "ays2026.firebaseapp.com",
+  projectId: "ays2026",
+  storageBucket: "ays2026.firebasestorage.app",
+  messagingSenderId: "776704252344",
+  appId: "1:776704252344:web:a81ed6f18d8fbd862dd74d",
+  measurementId: "G-3DZ9JGJVVM",
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
